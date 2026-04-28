f1f5ea9ceebd8928e7731bad73a6af0517167780
tankkevanshi <tankkevanshi@gmail.com>
2026-04-28 12:43:52 +0530

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,21 @@
+# print number from 1 to 10
+
+for i in range (1,11):
+    print (i)
+
+# print even numbers
+
+for i in range (1,21):
+    if i % 2 ==0:
+        print (i)
+
+# sum of first N Natural numbers
+
+n = 10
+total = 0
+for i in range (1,n+1):
+    total += i
+print ("sum:,total")
+
+
+
