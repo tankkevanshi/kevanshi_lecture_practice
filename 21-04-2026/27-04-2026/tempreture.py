950f3b9b7bbb0893689d91116127897d5786d571
tankkevanshi <tankkevanshi@gmail.com>
2026-04-29 10:38:27 +0530

    Add files via upload

---
+++ 21-04-2026/27-04-2026/tempreture.py
@@ -0,0 +1,7 @@
+Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+========================================================================================== RESTART: D:/kt/21-04-2026/27-04-2026/tempreture.py ==========================================================================================
+Convert (1)celsius to Fahrenheit or (2) Fahrenheit to celsius?Enter 1 or 2:1
+Enter temperature in Celsius:12
+Fahrenheit: 53.6
