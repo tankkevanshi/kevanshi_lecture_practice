bbdf547598672e704402989ffb8dc8bb3cf9f3e7
tankkevanshi <tankkevanshi@gmail.com>
2026-04-29 10:39:52 +0530

    Add files via upload

---
+++ kevanshi_python_lecture_practice/21-04-2026/python.py
@@ -0,0 +1,43 @@
+# Phython String Manipulation
+
+S1 = 'Hello'
+S2 = "World"
+S3 = '''multiline
+String'''
+S4 = r"Raw \n string"
+
+print (S1)
+print (S2)
+print (S3)
+print (S4)
+
+# Common String Method
+S = "Hello , World!"
+
+print (S.upper())
+print (S.lower())
+print (S.split())
+print (S.endswith("!"))
+print (S.startswith("H"))
+print (S.find("Hello"))
+print (S.count("L"))
+
+# starting Formating
+name = "Alice"
+age = 30
+
+# F-String
+print (S [0])
+print (S [9])
+print (S [0:5])
+print (S [: :-1])
+
+# Joining and spilitting in python
+
+words = ["python "," is ", "awesome"]
+print (" ".join (words))
+print ("_".join (words))
+
+Splits = "a,b,c"
+
+print (Splits.split(","))
